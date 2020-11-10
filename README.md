5103fd1d268678ad51021190dc933520de8a7eeb
Gilles van den Hoven <gilles@webunity.nl>
2020-11-10 14:49:32 +0100

    Added remaining nodes

--- README.md
+++ README.md
@@ -11,32 +11,28 @@ _In case remote API functionality is needed, feel free to submit a pull request 
 Run the following command in your Node-RED user directory - typically `~/.node-red`
 
 ```bash
-$ npm install node-red-contrib-slide.store
+$ npm install node-red-contrib-slide
 ```
 
 ## Getting started
 
 _The Slide Local API is, in it's current form, intended as a first version, and expected to undergo significant expansions in later updates. The focus of the current API is to allow you to directly control the curtains._
 
-By default, this HTTP API is disabled since it is not yet officially published by Innovation In Motion. I am a beta tester and have therefore access, but I will not expose how to enable to local API until i get the green light from IIM.
+By default, the local HTTP API is disabled since it is not yet officially published by Innovation In Motion. **I am a beta tester and have therefore access, but I will not expose how to enable to local API until i get the green light from the Slide team at Innovation In Motion.**
 
+## Available nodes
 
 The following nodes are implemented which expose a part of the Local API:
 
-- slide-conf
-	- Contains the `hostaneme` and the `device code` of a single slide.
-- slide-get-info
-	- Returns information about a Slide (e.g. the Device ID (MAC), the current position, whether Touch&Go is on or off, the name, the group name etc.)
+![Available nodes](https://github.com/gvdhoven/node-red-contrib-slide/blob/main/assets/readme/img/available-nodes.png?raw=true)
 
-## Using the (local) API
+### slide-conf
 
-Currently the local API does not yet support discovery (such as dns-sd, zeroconf etc.) so you will have to identify which local IP address your Slide is using. Due to a known bug all Slides currently have the hostname `espressif` on the local network, which might make this exercise slightly annoying if you have many Slides online.
+You need to add one configuration node per Slide motor you have installed. Currently the local API does not yet support discovery (such as dns-sd, zeroconf etc.) so you will have to identify which local IP address your Slide is using. Due to a known bug all Slides currently have the hostname `espressif` on the local network, which might make this exercise slightly annoying if you have many Slides online.
 
-### slide.conf
+![slide-conf](https://github.com/gvdhoven/node-red-contrib-slide/blob/main/assets/readme/img/slide-conf.png?raw=true)
 
-You need to add one configuration node per Slide motor you have installed. A filled in sample looks like this.
-
-![Sample config](https://github.com/gvdhoven/node-red-contrib-slide/blob/main/assets/readme/img/slide.conf.png?raw=true)
+Please enter at least the hostname and a device-code.
 
 As you can see, it contains a 'calibrate' button. Once you have entered the hostname and the devicecode, you can optionally click this button. This starts a custom calibration procedure:
 	* Starts the calibration procedure and waits until the curtain stops moving
@@ -45,9 +41,39 @@ As you can see, it contains a 'calibrate' button. Once you have entered the host
 	* Saves the 'open' offset (this should be near the 0.0 range)
 	* Restores the curtain to the approximate position it was while calibration was initiated.
 
-This offset is then used on subsequent calls on the `slide.setposition` node, to more precisely determine the curtain position and to see if an actual call to the motor is even needed.
+This offset is then used on subsequent calls on the `slide-set-calibrated-position` node, to more precisely determine the curtain position and to see if an actual call to the motor is even needed.
 
 For example;
 	- If the open offset is actually 0.09 and the closed offset 0.87 (just as an example) setting the curtain to 50% would mean that the curtain should move to position: 0.09 + (0.5 * (0.87 - 0.09)) = 0.48 (which is exactly halfway between 0.09 and 0.87.
 	- If the open offset is actually 0.09 and the curtain is already at 0.09; no `setPosition` command will be issued if you want to open the curtain; meaning you won't hear the motor wirring.
 	- If the close offset is actually 0.87 and the curtain is already at 0.87; no `setPosition` command will be issued if you want to close the curtain; meaning you won't hear the motor wirring.
+
+### slide-get-info
+
+Use this node in order to get information about the state of a certain Slide. This functionality is used heavily under the hood in order to determine calibration positions and to see when a certain Slide has stopped moving.
+
+![slide-get-info](https://github.com/gvdhoven/node-red-contrib-slide/blob/main/assets/readme/img/slide-get-info.png?raw=true)
+
+### slide-set-absolute-position
+
+This node is able to set an absolute position on the slide, from 0 to 100%. This will most probably not mean that your curtain is half-way closed if you pass in the following payload `{ "payload": { "percent": 50 } }`, due to the fact that the Slide at `open` position can actually be at e.g. 4% and that same curtain can report a closed state of 98%. Halfway in this case would then be 51%. This math is done for you if you take the next Node; `slide-set-calibrated-position`.
+
+### slide-set-calibrated-position
+
+This node takes the calibration settings found by clicking the 'Calibrate' button in the config node and applies that to a percentage offset. In the example shown at the `slide-set-absolute-position` node, the same payload `{ "payload": { "percent": 50 } }` would result in the Slide moving to 51%.
+
+### slide-open
+
+Opens a Slide. The behaviour can be tested using the 'stop' button while editing the `slide-conf` node.
+
+### slide-close
+
+Closes a Slide. The behaviour can be tested using the 'stop' button while editing the `slide-conf` node.
+
+### slide-stop
+
+Stops any motor movement of a Slide. The behaviour can be tested using the 'stop' button while editing the `slide-conf` node.
+
+### slide-update-wifi
+
+Allows you to update the WiFi SSID and password for a given Slide. Use this one with caution as it will result in loss of controlling the Slide if you pass in the wrong values.
--- slide.js
+++ slide.js
@@ -16,9 +16,15 @@ module.exports = function(RED) {
 		clearTimeout(node.statusTimer);
 		node.status({ 'fill': (ok ? 'green' : 'red'), 'shape': 'ring', 'text': msg });
 		node.statusTimer = setTimeout(() => {
-				node.status({ 'fill': 'blue', 'shape': 'ring', 'text': node.slide.hostname });
+				if (!node.slide.localApi.appearsValid()) {
+					node.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+				} else {
+					node.status({ 'fill': 'blue', 'shape': 'ring', 'text': node.slide.hostname });
+				}
 			}, 3000);
 	}
+
+
 
 	/**
 	 * Configuration node which holds only the device code.
@@ -41,12 +47,13 @@ module.exports = function(RED) {
 	RED.nodes.registerType("slide-conf", localConf);
 
 
+
 	/**
 	 * Get info node.
 	 *
 	 * @param {Node} node The node to create.
 	 */
-	function getInfo(n) {
+	function slideGetInfo(n) {
 		RED.nodes.createNode(this, n);
 		this.slide = RED.nodes.getNode(n.slide);
 		if (!this.slide) {
@@ -54,7 +61,11 @@ module.exports = function(RED) {
 		}
 
 		// Set hostname under slide
-		this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
 
 		var node = this;
 		node.on('input', (msg, send, done) => {
@@ -70,7 +81,287 @@ module.exports = function(RED) {
 			});
 		});
 	}
-	RED.nodes.registerType("slide-get-info", getInfo);
+	RED.nodes.registerType("slide-get-info", slideGetInfo);
+
+
+
+	/**
+	 *Set absolute position node
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideSetAbsolutePosition(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			var position = -1;
+			if (msg && msg.payload && msg.payload.hasOwnProperty('percent')) {
+				position = parseInt(msg.payload.percent);
+				if ((position === NaN) || (position < 0) || (position > 100)) {
+					position = -1;
+				} else  {
+					// If still a valid percentage, convert it to a double
+					position = (position / 100);
+				}
+			}
+
+			if (position === -1) {
+				blinkStatus(node, false, 'Invalid input');
+				node.warn('Please pass a message with a payload which looks like this: { "payload": { "percent": 50 } }');
+				send(null);
+				done();
+			} else {
+				node.slide.localApi.setPos(position).then(result => {
+					blinkStatus(node, true, 'Command OK');
+					send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+					done();
+				}).catch(e => {
+					blinkStatus(node, false, e.title);
+					node.warn(e.message);
+					send(null);
+					done();
+				});
+			}
+		});
+	}
+	RED.nodes.registerType("slide-set-absolute-position", slideSetAbsolutePosition);
+
+
+
+	/**
+	 * Set calibrated position node
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideSetCalibratedPosition(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			var position = -1;
+			if (msg && msg.payload && msg.payload.hasOwnProperty('percent')) {
+				position = parseInt(msg.payload.percent);
+				if ((position === NaN) || (position < 0) || (position > 100)) {
+					position = -1;
+				} else  {
+					// If still a valid percentage, convert it to double, taking into account the a calibrated position
+					position = ((node.slide.localApi.closedPosition - node.slide.localApi.openPosition) * (position / 100)) + node.slide.localApi.openPosition;
+				}
+			}
+
+			if (position === -1) {
+				blinkStatus(node, false, 'Invalid input');
+				node.warn('Please pass a message with a payload which looks like this: { "payload": { "percent": 50 } }');
+				send(null);
+				done();
+			} else if ((node.slide.localApi.closedPosition - node.slide.localApi.openPosition) < 0 ||
+					   (node.slide.localApi.closedPosition <= node.slide.localApi.openPosition)) {
+				blinkStatus(node, false, 'Invalid calibration detected.');
+				node.warn('Please re-run the calibration procedure.');
+				send(null);
+				done();
+			} else {
+				node.slide.localApi.setPos(position).then(result => {
+					blinkStatus(node, true, 'Command OK');
+					send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+					done();
+				}).catch(e => {
+					blinkStatus(node, false, e.title);
+					node.warn(e.message);
+					send(null);
+					done();
+				});
+			}
+		});
+	}
+	RED.nodes.registerType("slide-set-calibrated-position", slideSetCalibratedPosition);
+
+
+
+	/**
+	 * Open node.
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideOpen(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			node.slide.localApi.open().then(result => {
+				blinkStatus(node, true, 'Command OK');
+				send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+				done();
+			}).catch(e => {
+				blinkStatus(node, false, e.title);
+				node.warn(e.message);
+				send(null);
+				done();
+			});
+		});
+	}
+	RED.nodes.registerType("slide-open", slideOpen);
+
+
+
+	/**
+	 * Close node.
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideClose(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			node.slide.localApi.close().then(result => {
+				blinkStatus(node, true, 'Command OK');
+				send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+				done();
+			}).catch(e => {
+				blinkStatus(node, false, e.title);
+				node.warn(e.message);
+				send(null);
+				done();
+			});
+		});
+	}
+	RED.nodes.registerType("slide-close", slideClose);
+
+
+
+	/**
+	 * Stop node.
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideStop(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			node.slide.localApi.stop().then(result => {
+				blinkStatus(node, true, 'Command OK');
+				send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+				done();
+			}).catch(e => {
+				blinkStatus(node, false, e.title);
+				node.warn(e.message);
+				send(null);
+				done();
+			});
+		});
+	}
+	RED.nodes.registerType("slide-stop", slideStop);
+
+
+
+
+	/**
+	 * Updates the slide WiFi
+	 *
+	 * @param {Node} node The node to create.
+	 */
+	function slideUpdateWiFi(n) {
+		RED.nodes.createNode(this, n);
+		this.slide = RED.nodes.getNode(n.slide);
+		if (!this.slide) {
+			return null;
+		}
+
+		// Set hostname under slide
+		if (!this.slide.localApi.appearsValid()) {
+			this.status({ 'fill': 'red', 'shape': 'ring', 'text': 'Invalid config' });
+		} else {
+			this.status({ 'fill': 'blue', 'shape': 'ring', 'text': this.slide.hostname });
+		}
+
+		var node = this;
+		node.on('input', (msg, send, done) => {
+			var ssid = '';
+			var pass = '';
+			if (msg && msg.payload && msg.payload.hasOwnProperty('ssid') && msg.payload.hasOwnProperty('pass')) {
+				ssid = msg.payload.ssid.trim();
+				pass = msg.payload.pass.trim();
+			}
+
+			if ((ssid === '') || (pass === '')) {
+				blinkStatus(node, false, 'Invalid input');
+				node.warn('Please pass a message with a payload which looks like this: { "payload": { "ssid": "your_new_ssid", "pass": "your_new_ssid_password" } }');
+				send(null);
+				done();
+			} else {
+				node.slide.localApi.updateWifi(ssid, pass).then(result => {
+					blinkStatus(node, true, 'Command OK');
+					send({ 'topic': ((node.topic !== '') ? node.topic : 'slide'), 'payload': result });
+					done();
+				}).catch(e => {
+					blinkStatus(node, false, e.title);
+					node.warn(e.message);
+					send(null);
+					done();
+				});
+			}
+		});
+	}
+	RED.nodes.registerType("slide-update-wifi", slideUpdateWiFi);
+
 
 
 	// HTTP callbacks for config node
@@ -95,15 +386,6 @@ module.exports = function(RED) {
 	RED.httpAdmin.post('/slide/open', function(req, res) {
 		var localApi = new LocalApi(req.body.hostname, req.body.devicecode, req.body.openPosition, req.body.closePosition);
 		localApi.open().then(result => {
-			res.status(200).send(result);
-		}).catch(e => {
-			res.status(e.code).send(e);
-		});
-	});
-
-	RED.httpAdmin.post('/slide/position', function(req, res) {
-		var localApi = new LocalApi(req.body.hostname, req.body.devicecode, req.body.openPosition, req.body.closePosition);
-		localApi.setPos(req.body.pos).then(result => {
 			res.status(200).send(result);
 		}).catch(e => {
 			res.status(e.code).send(e);
